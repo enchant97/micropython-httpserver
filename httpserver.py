4dcc2c7dda17ff33c794e1beeb19368f38bc47ff
Leo Spratt <enchant97@users.noreply.github.com>
2022-12-22 17:08:21 +0000

    add percent decode func

--- httpserver.py
+++ httpserver.py
@@ -12,6 +12,22 @@ DEFAULT_CLIENT_HEADERS = {
 }
 
 
+def perc_decode(v, from_form=False):
+    decoded = b""
+    i = 0
+    while i < len(v):
+        if v[i] == "%":
+            encoded = v[i+1:i+3]
+            decoded += bytes.fromhex(encoded)
+            i += 2
+        elif from_form and v[i] == "+":
+            decoded += b" "
+        else:
+            decoded += v[i].encode()
+        i += 1
+    return decoded
+
+
 async def readuntil(reader, separator):
     buffer = b""
     while (char := await reader.read(1)):
